79075178dce78462f6cbdaa08fe7efb66295fcae
Steve Hatch <laughsinacan@gmail.com>
2017-11-16 10:36:33 -0700

    added castle and grass, moved attack buttons onto charinfowindow

---
+++ Assets/Scripts/UI/GenerateGrass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateGrass : MonoBehaviour
+{
+	public GameObject grass;
+
+	public void Awake()
+	{
+		int sizeX = 12;
+		int sizeY = 5;
+		for(int i = -sizeX; i <= sizeX; i++)
+		{
+			for(int j = -sizeY; j <= sizeY; j++)
+			{
+				Instantiate(grass, new Vector3(i, j, 0), Quaternion.identity);
+			}
+		}
+	}
+}
+
+
